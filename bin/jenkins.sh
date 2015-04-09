5c7e44d898d7cf50fc9361142fb3a865beccb170
Fabien Arcellier <farcellier@octo.com>
2015-04-09 14:22:36 +0200

    Ansible script to deploy jenkins on localhost.
    This installation is unsecure.

---
+++ bin/jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_PATH="${DIRECTORY}/.."
+cd "${ROOT_PATH}"
+
+# Deploy jenkins server
+export PYTHONUNBUFFERED=1
+ansible-playbook -i "inventory" "jenkins.yml"  @*
